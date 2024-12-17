60e1041ba7c9d7ab5731d74812980996218bdf8b
K900 <me@0upti.me>
2024-12-17 21:27:43 +0300

    mesa-radeonsi-jupiter: refresh patches for 24.3.1

--- pkgs/mesa-radeonsi-jupiter/default.nix
+++ pkgs/mesa-radeonsi-jupiter/default.nix
@@ -1,21 +1,16 @@
 { mesa, fetchpatch }:
-# Patches from mesa-24.0..radeonsi-24.0.4, minus things merged in Mesa upstream
+# Patches from nixpkgs mesa..vendor radeonsi branch
 mesa.overrideAttrs(old: {
   patches = old.patches ++ [
-    # Cherry-pick the swapchain override bits from the Valve 24.1 branch
+    # Cherry-pick the swapchain override bits from the Valve 24.3 branch
     (fetchpatch {
-      url = "https://github.com/Jovian-Experiments/mesa/commit/d0722142079fdc5dab999aca9456cab6b8a9a214.patch";
-      hash = "sha256-oayXr+BjgO47yZM1IXnpykDTNirqZBW75nN/wsRzNGw=";
+      url = "https://github.com/Jovian-Experiments/mesa/commit/27482ce2030cc4249908382fb4e2092134135fdb.patch";
+      hash = "sha256-tQoGN/88sdPDQmscfkPKulPasU2tLeRekWG9xWF/BsQ=";
     })
     # Disable glthread by default
     (fetchpatch {
       url = "https://github.com/Jovian-Experiments/mesa/commit/09d9c2fd7f69fbe59aab0bb53438d3446ea90dd4.patch";
       hash = "sha256-y0wGWVBQiWd2hOqiqDIKP9VERNSP4owuPGjnqUTNy68=";
-    })
-    # Backport HEVC encode fix
-    (fetchpatch {
-      url = "https://github.com/Jovian-Experiments/mesa/commit/6c30d44613d5945c0660701a5a59c659a709ca37.patch";
-      hash = "sha256-pl1gU6aMUupFAf4vxDffzpyZ3Lz6SzVYeAtLuulLWcU=";
     })
   ];
 })
