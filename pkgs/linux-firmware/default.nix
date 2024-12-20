3be0f3e47c757f3f6e63961cec52a8133249a205
K900 <me@0upti.me>
2024-12-20 10:06:57 +0300

    linux-firmware-jupiter: 20240917.1 -> 20241205.1

--- pkgs/linux-firmware/default.nix
+++ pkgs/linux-firmware/default.nix
@@ -1,13 +1,13 @@
 { linux-firmware, fetchFromGitHub }:
 
 linux-firmware.overrideAttrs(_: rec {
-  version = "20240917.1";
+  version = "20241205.1";
 
   src = fetchFromGitHub {
     owner = "Jovian-Experiments";
     repo = "linux-firmware";
     rev = "jupiter-${version}";
-    hash = "sha256-mqBREldC1/nRzjDyPCneeMhDSW4w4VTH2K4F2IDYVxo=";
+    hash = "sha256-Ejww1PK8hKjoRiHt/CEz2sS9MSV2ZoMaFYpZPGJCFEA=";
   };
 
   # FIXME: workaround newer linux-firmware needing `install dedup`
